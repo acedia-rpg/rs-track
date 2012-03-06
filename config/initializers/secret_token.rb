0d3f313b11cd9d4565d7a06c10d01a8a46fc258d
Gary Allison <acedia.rpg@gmail.com>
2012-03-05 16:17:37 -0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RsTracker::Application.config.secret_token = 'c66781b804485f269b4f8da7bd62306d861f85222aa7f209b6dfbbe2aab592199e924429e0660ab47353f7537c75da46b70e21088014c0a88eaa5f9678cec4b8'
